73e0fba43cf2006fc569f549962771b625157160
Prince <prince@azket.com>
2019-01-17 22:12:13 +0530

    Updates

---
+++ precache-manifest.57d38f3a5549507be71a48004feec982.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "4b9618b264c7a7410c17",
+    "url": "/contactmanager/static/js/main.4b9618b2.chunk.js"
+  },
+  {
+    "revision": "e2a8319f35e55f47af7b",
+    "url": "/contactmanager/static/js/1.e2a8319f.chunk.js"
+  },
+  {
+    "revision": "4b9618b264c7a7410c17",
+    "url": "/contactmanager/static/css/main.948eaa6c.chunk.css"
+  },
+  {
+    "revision": "e2a8319f35e55f47af7b",
+    "url": "/contactmanager/static/css/1.0655359f.chunk.css"
+  },
+  {
+    "revision": "9f676c144f1a9fbca594e75dcd94b0ab",
+    "url": "/contactmanager/index.html"
+  }
+];
